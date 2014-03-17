42b4899f994080b271b9fed8b5ad451414ee9ba6
555hamano <555hamano@gmail.com>
2014-03-18 02:25:57 +0900

    add a sql file.

---
+++ make.sql
@@ -0,0 +1,30 @@
+CREATE TABLE `age_ranges` (
+  `id` int(10) unsigned NOT NULL auto_increment,
+  `min` tinyint(3) unsigned NOT NULL,
+  `max` tinyint(3) unsigned NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB;
+
+CREATE TABLE `campaigns` (
+  `id` int(10) unsigned NOT NULL auto_increment,
+  `sex` tinyint(3) unsigned NOT NULL,
+  `age_range_id` tinyint(3) unsigned NOT NULL,
+  `banner` varchar(512) NOT NULL,
+  `lp_url` varchar(1023) NOT NULL,
+  `cpm` int(11) default '0',
+  `cpc` int(11) default '0',
+  `ecpm` int(10) unsigned default '0',
+  `budget` int(10) unsigned default '0',
+  PRIMARY KEY  (`id`),
+  KEY `age_range_id` (`age_range_id`)
+) ENGINE=InnoDB
+;
+
+CREATE TABLE `demographics` (
+  `uuid` int(10) unsigned NOT NULL auto_increment,
+  `sex` tinyint(3) unsigned NOT NULL default '0',
+  `age` tinyint(3) unsigned NOT NULL default '0',
+  PRIMARY KEY  (`uuid`)
+) ENGINE=InnoDB
+;
+
